dcd4b41a9d6d4f2b185fd16419e04240655ce4f6
Kim Westerholm <kim.westerholm@extreme.consulting>
2021-05-20 21:13:00 +0300

    form style improvements
    added icon library to project
    converted passengers and flight class inputs to menus
    improved form styles

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,18 @@
 import React, { useContext } from 'react'
-import { Input, Box, HStack, Text, VStack, ButtonGroup, Button } from '@chakra-ui/react'
+import {
+    Input,
+    Box,
+    HStack,
+    Text,
+    VStack,
+    ButtonGroup,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem
+} from '@chakra-ui/react'
+import { CheckCircleIcon, ChevronDownIcon } from '@chakra-ui/icons'
 import './Form.css'
 import { FlightDataContext } from '../../context/flightDataContext'
 import { useSpeechContext } from '@speechly/react-client'
@@ -12,17 +25,26 @@ export default function Form(): JSX.Element {
     const { flightData, tentativeFlightData } = useContext(FlightDataContext)
     const formData = speechState === 'Recording' || speechState === 'Loading' ? tentativeFlightData : flightData
 
+    const getPassengerMenuItems = () => {
+
+        const items = []
+        for (let i = 1; i < 10; i++) {
+            items.push(<MenuItem key={i} >{i}</MenuItem>)
+        }
+        return items
+    }
+
     return (
         <Box
             p={30}
             display='flex'
             flexDirection='column'
             alignItems='center'
-            bgGradient="linear(to-r, blue.500, teal.50)"
+            bgGradient="linear(to-r, blue.400, teal.50)"
             borderWidth='1px'
             borderRadius='5px'
             w='900px'
-            h='400px'>
+            h='500px'>
             <ButtonGroup spacing={0} display='flex' width='600px'>
                 <Button
                     flex={1}
@@ -53,7 +75,7 @@ export default function Form(): JSX.Element {
                 <VStack>
                     <div className='inputWrapper'>
                         <Text className='inputLabel'>From</Text>
-                        <Input id='from-input' className='input' variant='unstyled' value={formData?.from} size='lg' />
+                        <Input fontSize='28px' id='from-input' className='input' variant='unstyled' value={formData?.from} size='lg' />
                     </div>
                     <div className='inputWrapper'>
                         <Text className='inputLabel'>Departure</Text>
@@ -61,6 +83,7 @@ export default function Form(): JSX.Element {
                             KeyboardButtonProps={{
                                 className: 'icon'
                             }}
+                            format='mm/dd/yyyy'
                             className='dateInput'
                             disablePast
                             variant='inline'
@@ -71,13 +94,21 @@ export default function Form(): JSX.Element {
                     </div>
                     <div className='inputWrapper'>
                         <Text className='inputLabel'>Passengers</Text>
-                        <Input id='passengers-input' className='input' variant='unstyled' value={formData?.passengers} size='lg' />
+                        <Menu>
+                            <MenuButton>
+                                {formData?.passengers}
+                                <ChevronDownIcon />
+                            </MenuButton>
+                            <MenuList>
+                                {getPassengerMenuItems()}
+                            </MenuList>
+                        </Menu>
                     </div>
                 </VStack>
                 <VStack>
                     <div className='inputWrapper'>
                         <Text className='inputLabel'>To</Text>
-                        <Input id='to-input' className='input' variant='unstyled' value={formData?.to} size='lg' />
+                        <Input fontSize='28px' id='to-input' className='input' variant='unstyled' value={formData?.to} size='lg' />
                     </div>
                     <div className='inputWrapper'>
                         <Text className='inputLabel'>Return</Text>
@@ -87,6 +118,7 @@ export default function Form(): JSX.Element {
                             }}
                             autoOk
                             className='dateInput'
+                            format='mm/dd/yyyy'
                             disablePast
                             variant='inline'
                             onChange={(d: Date | null) => console.log(d)}
@@ -95,10 +127,24 @@ export default function Form(): JSX.Element {
                     </div>
                     <div className='inputWrapper'>
                         <Text className='inputLabel'>Class</Text>
-                        <Input id='class-input' className='input' variant='unstyled' value={formData?.class} size='lg' />
+                        <Menu>
+                            <MenuButton>
+                                {formData?.class}
+                                <ChevronDownIcon />
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>Economy</MenuItem>
+                                <MenuItem>Business</MenuItem>
+                                <MenuItem>First Class</MenuItem>
+                            </MenuList>
+                        </Menu>
                     </div>
                 </VStack>
             </HStack>
+            <div className='checkboxWrapper'>
+                <CheckCircleIcon className='checkbox' color='#0f4e92' w={16} h={16} />
+                <Text className='checkboxLabel'>Direct Only</Text>
+            </div>
         </Box>
     )
 }
