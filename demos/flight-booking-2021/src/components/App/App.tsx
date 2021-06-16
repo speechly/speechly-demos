6306f0306629fa1bbe9ebf65718989dd5f2dfffe
Kim Westerholm <kim.westerholm@extreme.consulting>
2021-06-16 14:03:22 +0300

    demo and hero mode implementations

--- demos/flight-booking-2021/src/components/App/App.tsx
+++ demos/flight-booking-2021/src/components/App/App.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { SpeechProvider, SpeechSegment, SpeechState, useSpeechContext } from '@speechly/react-client'
+import {
+  SpeechProvider,
+  SpeechSegment,
+  SpeechState,
+  useSpeechContext
+} from '@speechly/react-client'
 import {
   PushToTalkButton,
   PushToTalkButtonContainer,
@@ -7,6 +12,7 @@ import {
 } from '@speechly/react-ui'
 import { TranscriptDrawer } from '@speechly/react-ui/components/TranscriptDrawer'
 import { startDemo, stopDemo } from '@speechly/browser-ui/demomode'
+import { useLocation } from 'react-router'
 
 import LuxonUtils from '@date-io/luxon'
 import { ChakraProvider, Center } from '@chakra-ui/react'
@@ -15,6 +21,8 @@ import FlightContextProvider from '../../context/flightDataContext'
 
 
 import Form from '../Form/Form'
+import { useUpdateFlightData } from '../../hooks/useUpdateFlightData'
+
 
 const UsageHints = [
   'Try: "Book a return flight from London to New York"',
@@ -28,7 +36,7 @@ const UsageHints = [
 ]
 
 const DemoStrings = [
-  '*book Book a return flight from London(from) to New York(to)',
+  '*book Book a return flight from London(from) to [New York](to)',
   '*clear clear',
 ]
 
@@ -38,7 +46,7 @@ const App: React.FC = (): JSX.Element => {
       <MuiPickersUtilsProvider utils={LuxonUtils}>
         <ChakraProvider>
           <SpeechProvider appId="1ea63538-f95c-4259-b8af-923994424137" language="en-US">
-            <SpeechlyApp/>
+            <SpeechlyApp />
           </SpeechProvider>
         </ChakraProvider>
       </MuiPickersUtilsProvider>
@@ -48,7 +56,24 @@ const App: React.FC = (): JSX.Element => {
 
 const SpeechlyApp: React.FC = (): JSX.Element => {
   const [mockSegment, setMockSegment] = useState<SpeechSegment | undefined>()
+  const [demoMode, setDemoMode] = useState(true)
   const { speechState } = useSpeechContext()
+
+  function useQuery() {
+    return new URLSearchParams(useLocation().search)
+  }
+
+
+  window.addEventListener('message', (e) => {
+    if (e.data.type === 'interactive') {
+      setDemoMode(false)
+      stopDemoMode()
+    }
+  })
+
+  const query = useQuery()
+  const heroMode = query.get('hero') === 'true'
+  const hidePushToTalkButton = heroMode && demoMode
 
   useEffect(() => {
     startDemoMode()
@@ -57,6 +82,7 @@ const SpeechlyApp: React.FC = (): JSX.Element => {
   useEffect(() => {
     if (speechState === SpeechState.Recording) {
       stopDemoMode()
+      setDemoMode(false)
     }
   }, [speechState])
 
@@ -72,7 +98,9 @@ const SpeechlyApp: React.FC = (): JSX.Element => {
   const stopDemoMode = () => {
     stopDemo()
   }
-  
+
+  useUpdateFlightData(mockSegment)
+
   return (
     <>
       <TranscriptDrawer
@@ -83,10 +111,12 @@ const SpeechlyApp: React.FC = (): JSX.Element => {
         smallTextColor='#187ce7'
         backgroundColor='rgba(162, 213, 240, 0.4)' />
 
-      <PushToTalkButtonContainer>
-        <PushToTalkButton captureKey="" intro="Hold to book a flight with voice" showTime={30000} />
-        <ErrorPanel />
-      </PushToTalkButtonContainer>
+      {!hidePushToTalkButton &&
+        <PushToTalkButtonContainer>
+          <PushToTalkButton captureKey="" intro="Hold to book a flight with voice" showTime={30000} />
+          <ErrorPanel />
+        </PushToTalkButtonContainer>
+      }
 
       <Center
         display='flex'
