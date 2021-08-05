cd8538242f791f0867f7cd11b73922cc46cc43fa
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-08-05 10:59:34 +0300

    Layout tweaks. Preliminary text/voice edit capabilities

--- demos/speech-to-text/src/components/App/App.tsx
+++ demos/speech-to-text/src/components/App/App.tsx
@@ -1,53 +1,57 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { SyntheticEvent, useContext, useEffect, useState } from 'react'
 import { SpeechProvider, useSpeechContext } from '@speechly/react-client'
 import {
-    PushToTalkButton,
-    PushToTalkButtonContainer,
-    ErrorPanel
+  PushToTalkButton,
+  PushToTalkButtonContainer,
+  ErrorPanel
 } from '@speechly/react-ui'
-import { TranscriptDrawer } from '@speechly/react-ui/components/TranscriptDrawer'
+import './App.css'
 
 const App: React.FC = (): JSX.Element => {
-
-    const UsageHints = [
-        'Add usage hints',
-    ]
-
-    return (
-        <SpeechProvider appId="6f1c7eaa-53fa-495e-9319-4ceacfa88cfe" language="en-US">
-            <PushToTalkButtonContainer>
-                <PushToTalkButton captureKey="" intro="" showTime={30000} />
-                <ErrorPanel />
-            </PushToTalkButtonContainer>
-
-            <SpeechlyApp></SpeechlyApp>
-
-        </SpeechProvider>
-    )
+  
+  return (
+    <SpeechProvider appId="6f1c7eaa-53fa-495e-9319-4ceacfa88cfe" language="en-US">
+    <PushToTalkButtonContainer>
+    <PushToTalkButton captureKey=" " intro="Hold to dictate" showTime={30000} />
+    <ErrorPanel />
+    </PushToTalkButtonContainer>
+    
+    <SpeechlyApp/>
+    
+    </SpeechProvider>
+  )
 }
-
+  
 const SpeechlyApp: React.FC = (): JSX.Element => {
-
-    const [textContent, setTextContent] = useState<string>("");
-    const [tentativeTextContent, setTentativeTextContent] = useState<string>("");
-    const { segment } = useSpeechContext();
-
-    useEffect(() => {
-        console.log(segment);
-        if (segment) {
-            const alteredTextContent = [textContent, ...segment.words.map(w => w.value)].join(" ")
-            setTentativeTextContent(alteredTextContent);
-            if (segment.isFinal) {
-                setTextContent(alteredTextContent);
-            }
-        }
-    }, [segment])
-
-    return (
-        <p contentEditable="true" style={{display: "block", fontSize: "2rem", width: "100%", height: "100vh"}}>
-            {tentativeTextContent}
-        </p>
-    )
+  
+  const [textContent, setTextContent] = useState<string>('')
+  const [tentativeTextContent, setTentativeTextContent] = useState<string>('')
+  const { segment } = useSpeechContext()
+  
+  const setText = (value: string) => {
+    setTextContent(value)
+    setTentativeTextContent(value)
+  }
+  
+  useEffect(() => {
+    console.log(segment)
+    if (segment) {
+      const plainString = segment.words.filter(w => w.value).map(w => w.value).join(' ')
+      const alteredTextContent = textContent ? [textContent, plainString].join(' ') : plainString
+      setTentativeTextContent(alteredTextContent)
+      if (segment.isFinal) {
+        setTextContent(alteredTextContent)
+      }
+    }
+  }, [segment])
+  
+  return (
+    <div className="pageMargin">
+      <main>
+        <textarea onChange={e => setText(e.target.value)} value={tentativeTextContent} />
+      </main>
+    </div>
+  )
 }
-
+    
 export default App
