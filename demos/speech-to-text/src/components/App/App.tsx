3531544d8ceadb7e87e888b8ff0555c56325ee0c
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-08-05 16:32:17 +0300

    Improved speech-to-text visuals

--- demos/speech-to-text/src/components/App/App.tsx
+++ demos/speech-to-text/src/components/App/App.tsx
@@ -11,8 +11,8 @@ const App: React.FC = (): JSX.Element => {
   
   return (
     <SpeechProvider appId="6f1c7eaa-53fa-495e-9319-4ceacfa88cfe" language="en-US">
-      <PushToTalkButtonContainer voffset="1.75rem">
-        <PushToTalkButton captureKey=" " intro="Hold to dictate" showTime={0} />
+      <PushToTalkButtonContainer voffset="calc(1.8rem + 4vh)" size="5rem">
+        <PushToTalkButton size="5rem" backgroundColor="#494287" captureKey=" " intro="Hold to dictate" showTime={0} />
         <ErrorPanel />
       </PushToTalkButtonContainer>
       
@@ -48,7 +48,7 @@ const SpeechlyApp: React.FC = (): JSX.Element => {
   return (
     <div className="pageMargin">
       <main>
-        <textarea placeholder="Hold the mic button to dictate" onChange={e => setText(e.target.value)} value={tentativeTextContent} />
+        <textarea placeholder=">" onChange={e => setText(e.target.value)} value={tentativeTextContent} />
       </main>
     </div>
   )
