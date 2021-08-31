448d58d8509f8523c108d359cc76177ced32ca44
Ari Nykänen <ari@speechly.com>
2021-08-31 16:30:55 +0300

    Removed date formatting

--- demos/flight-booking-2021/src/utils/dateUtils.ts
+++ demos/flight-booking-2021/src/utils/dateUtils.ts
@@ -5,34 +5,12 @@ function appendLeadingZeroes(n: number): string {
     return `${n}`
 }
 
-export function nextDate(dayIndex: number): Date {
-    const today = new Date()
-    const currentWeekdayIndex = today.getDay()
-    if (dayIndex === currentWeekdayIndex) return today
-
-    today.setDate(today.getDate() + (dayIndex - 1 - today.getDay() + 7) % 7 + 1)
-    return today
-}
-
 export function calculateDateEntity(dateString: string): string {
-    const now = new Date()
     const utteredDate = new Date(dateString)
 
-    let wantedTime
-    if (utteredDate > now) {
-        wantedTime = utteredDate
-    }
-    // if past date is uttered, calculate next date with 
-    // same weekday index
-    else {
-        const parsedDate = Date.parse(dateString)
-        const weekdayIndex = new Date(parsedDate).getDay()
-        wantedTime = nextDate(weekdayIndex)
-    }
-
-    const result = `${appendLeadingZeroes(wantedTime.getUTCMonth() + 1)}/${appendLeadingZeroes(
-        wantedTime.getUTCDate()
-    )}/${appendLeadingZeroes(wantedTime.getUTCFullYear())}`
+    const result = `${appendLeadingZeroes(utteredDate.getUTCMonth() + 1)}/${appendLeadingZeroes(
+        utteredDate.getUTCDate()
+    )}/${appendLeadingZeroes(utteredDate.getUTCFullYear())}`
 
     return result
 }
