c9e32d2199e3f90907a7e12df26136c20e4fa207
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-10-15 20:10:15 +0300

    Removed build artefacts from Git repo. Added to **/build and **/dist to .gitignore

--- libraries/voice-form-elements/dist/components/Calendar.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-declare type Props = {
-    date?: Date;
-    onDatePick?: (value: Date) => void;
-};
-export declare const Calendar: ({ date, onDatePick }: Props) => JSX.Element;
-export {};
